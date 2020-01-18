b9dc9900c0c9923c82f77e067cf2f8b896da42d7
MelaHub <pamela.gotti@gmail.com>
2020-01-18 16:52:39 +0100

    fixed untracked files

---
+++ chapter-3/exit.s
@@ -0,0 +1,19 @@
+# PURPOSE: simple program that exis and returns a status code back to the Linux kernel
+
+# INPUT:   none
+
+# OUTPUT:  returns a status code. This can be viewed by typing
+#          echo $?
+#          after running the program
+
+# VARIABLES:
+#           %eax holds the system call number
+#           %ebx holds the return status
+.section .data
+
+.section .text
+.globl _start
+_start:
+ movl $1, %eax # this is the linux kernel command number (system call) for exiting a program
+ movl $45, %ebx # this is the status numer we will retusrn to the operating system
+ int $0x80     # this wakes up the kernel to run the exit command
